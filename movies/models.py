5407a712d4199cb4daf82f5e110efceb21200066
조은누리 <jounuli@naver.com>
2021-11-17 15:26:47 +0900

    commit

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateField()
+    genres = models.ManyToManyField(Genre)
+    poster_path = models.CharField(max_length=200)
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
---
+++ movies/serializers.py
@@ -0,0 +1,102 @@
+from rest_framework import serializers
+from .models import Actor, Movie, Review
+
+# 배우
+class ActorSerializer(serializers.ModelSerializer):
+
+    class MovieSerializer(serializers.ModelSerializer):
+        
+        class Meta:
+            model = Movie
+            fields = ('pk', 'title', )
+
+    movie = MovieSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Actor
+        fields = ('name', 'movie', )
+
+
+class ActorListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = ('pk', 'name', )
+
+
+# 영화
+# class MovieSerializer(serializers.ModelSerializer):
+
+#     class ActorSerializer(serializers.ModelSerializer):
+
+#         class Meta:
+#             model = Actor
+#             fields = ('pk', 'name', )
+
+#     class ReviewSerializer(serializers.ModelSerializer):
+
+#         class Meta:
+#             model = Review
+#             fields = ('pk', 'title', )
+
+#     actor = ActorSerializer(many=True, read_only=True)
+#     review = ReviewSerializer(many=True, read_only=True)
+
+#     actor_pks = serializers.ListField(write_only=True)
+
+#     def create(self, validated_data):
+#         actor_pks = validated_data.pop('actor_pks')
+#         movie = Movie.objects.create(**validated_data)
+
+#         for actor_pk in actor_pks:
+#             movie.actor.add(actor_pk)
+
+#         return movie
+
+#     def update(self, movie, validated_data):
+#         actor_pks = validated_data.pop('actor_pks')
+
+#         for attr, value in validated_data.items():
+#             setattr(movie, attr, value)
+#             movie.save()
+
+#         movie.actor.clear
+#         for actor_pk in actor_pks:
+#             movie.actor.add(actor_pk)
+
+#         return movie
+
+#     class Meta:
+#         model = Movie
+#         fields = ('pk', 'title', 'overview',
+#          'release_date', 'poster_path', 'actor', 'review', 'actor_pks')
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ('pk', 'title', )
+
+
+# # 리뷰
+# class ReviewSerializer(serializers.ModelSerializer):
+
+#     class MovieSerializer(serializers.ModelSerializer):
+        
+#         class Meta:
+#             model = Movie
+#             fields = ('pk', 'title', )
+
+#     movie = MovieSerializer(read_only=True)
+
+#     class Meta:
+#         model = Review
+#         fields = ('pk', 'title', 'rank', 'content', 'movie', )
+
+
+# class ReviewListSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Review
+#         fields = ('pk', 'title', )
