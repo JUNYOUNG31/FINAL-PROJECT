c47f315ece807c9730dda65869f12bacc68515e7
JUNYOUNG <roywnsdud31@gmail.com>
2021-11-17 14:01:15 +0900

    기본적인 설치와 연결확인을 위한 작업

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'Signup' }">Signup</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+
+    }
+  },
+  // methods: {
+  //   logout: function() {
+  //     this.isLogin = false
+  //     localStorage.removeItem('jwt')
+  //     this.$router.push({name:'Login'})
+  //   }
+  // },
+  // created: function () {
+  //   const token = localStorage.getItem('jwt')
+
+  //   if(token) {
+  //     this.isLogin = true
+  //   }
+  // }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Signup from '@/views/accounts/Signup'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/accounts/signup',
+    name: 'Signup',
+    component: Signup,
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
