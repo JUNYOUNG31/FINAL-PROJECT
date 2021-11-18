ae510cfbb23f3b1b8fbeca0bf93fad8a90ee3843
JUNYOUNG <roywnsdud31@gmail.com>
2021-11-18 11:45:22 +0900

    vue에 영화 뿌리기중

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+// import router from '@/router'
 
+const SERVER_URL = 'http://127.0.0.1:8000/'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    movies: []
   },
   mutations: {
+    GET_MOVIES(state, res) {
+      state.movies = res
+    },
   },
   actions: {
+    getMovies({commit}, token) {
+      axios({
+        method: 'GET',
+        url: `${SERVER_URL}movies/`,
+        headers: token,
+      })
+      .then(res => {
+        commit('GET_MOVIES', res.data)
+        // commit('GET_MOVIE_TITLES', res.data)
+      })
+      .catch(err => console.log(err))
+    },
+  },
+  getters: {
+    movies(state) {
+      return state.movies
+    }
   },
   modules: {
   }
--- src/views/movies/MovieList.vue
+++ src/views/movies/MovieList.vue
@@ -1,9 +1,32 @@
 <template>
+  <div>
   <p>영화 리스트</p>
+  <p v-for="movie in movies" :key="movie.title">
+    {movie.title}
+  </p>
+  </div>
+  
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
+  name: 'MovieList',  
+  methods: {
+    setToken() {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        Authorization: `JWT ${token}`
+      }
+      return config
+    },
+  },
+  computed: {
+    ...mapGetters(['movies'])
+  },
+  created() {
+    this.$store.dispatch('getMovies', this.setToken())
+  },
 
 }
 </script>
