09e59810202721faf0b6756df70f8543c6348961
조은누리 <jounuli@naver.com>
2021-11-18 13:11:54 +0900

    detail url

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'movies'
+
 urlpatterns = [
     path('', views.getmovie),
+    path('<int:movie_pk>/', views.movie_detail),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,8 @@
+from django.http import response
 from django.http.response import HttpResponse
 from django.views.decorators.http import require_safe
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
 from .models import Movie
 from .serializers import MovieListSerializer
 from rest_framework.response import Response
@@ -13,3 +16,10 @@ def getmovie(request):
     movies = Movie.objects.all()
     serializer = MovieListSerializer(movies, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieListSerializer(movie)
+    return response(serializer.data)
