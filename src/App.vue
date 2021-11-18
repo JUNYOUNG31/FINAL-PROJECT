8e5e3cf94ea888d92e3c6a7d36700ca7533d9aca
조은누리 <jounuli@naver.com>
2021-11-18 20:40:15 +0900

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <v-app>
+
     <div id="nav">
       <span v-if="isLogin">
         <!-- <router-link :to="{ name: 'Profile' }">Profile</router-link>| -->
@@ -16,55 +17,17 @@
       </span>
     </div>
     <router-view @login="isLogin=true"/>
-  </div>
+
+  </v-app>
 </template>
 
-
 <script>
+
 export default {
   name: 'App',
-  data: function () {
-    return {
-      isLogin: false
-    }
-  },
-  methods: {
-    logout: function() {
-      this.isLogin = false
-      localStorage.removeItem('jwt')
-      this.$router.push({name:'MovieList'})
-    }
-  },
-  created: function () {
-    const token = localStorage.getItem('jwt')
 
-    if(token) {
-      this.isLogin = true
-    }
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/components/ReviewItem.vue
+++ src/components/ReviewItem.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>    
-    <hr>
-    <h2>{{ reviewItem.title }}</h2>   
-    <p>{{ reviewItem.movie_title.title }}</p>
-    <p>{{ reviewItem.rank }}</p>
-    <p>{{ reviewItem.content }}</p>
+  <div>
+    <v-expansion-panels>
+    <v-expansion-panel
+    >
+      <v-expansion-panel-header>
+        <h2 >리뷰 제목: {{ reviewItem.title }}</h2>   
+        <h2 >작성자: {{ reviewItem.user.username }}</h2>   
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>영화 제목: {{ reviewItem.movie_title.title }}</p>
+        <p>평점: {{ reviewItem.rank }}</p>
+        <p>리뷰 내용: {{ reviewItem.content }}</p>
+        <p>이 글을 좋아한 사람: {{ reviewItem.like_users }}</p>
+        <hr>
+        <p>작성 시각: {{ reviewItem.created_at }}</p>
+        <p>수정 시각: {{ reviewItem.updated_at }}</p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>    
   </div>
 </template>
 
@@ -24,8 +37,15 @@ export default {
         title: this.review.title,
         content: this.review.content,
         rank: this.review.rank,
+        created_at: this.review.created_at,
+        updated_at: this.review.updated_at,
+        user: this.review.user,
+        like_users: this.review.like_users
       },
     }
+  },
+  methods: {
+
   }
 }
 </script>
