a744683008baf4fc2582808164b23dd3843d7a43
조은누리 <jounuli@naver.com>
2021-11-21 16:58:02 +0900

    뭔진모르지만

--- movies/models.py
+++ movies/models.py
@@ -28,7 +28,7 @@ class Review(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
 
     def __str__(self):
